6617aca33fe6378d23bf77efd92a5e330a2b27e3
rishika251 <rishikasadineni@gmail.com>
2022-11-10 11:34:21 +0530

    Added:create user data details

---
+++ UserData.java
@@ -0,0 +1,49 @@
+package com.bridgelabz.AddressBook;
+
+public class UserData {
+	//declare user contact details data
+    String firstName;
+    String lastName;
+    long phoneNumber;
+    String emailID;
+    String address;
+    String cityName;
+    String stateName;
+    int zipCode;
+
+
+
+    //Constructor created
+    public UserData(String firstName, String lastName, long phoneNumber, String emailID, String address, String cityName, String stateName, int zipCode) {
+    //assign parameter
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.emailID = emailID;
+        this.address = address;
+        this.cityName = cityName;
+        this.stateName = stateName;
+        this.zipCode = zipCode;
+    }
+
+
+    public String toString() {
+        return "Address Book ::  " +
+                " " + firstName +
+                " " + lastName +
+                " " + phoneNumber +
+                " " + emailID +
+                " " + address +
+                " " + cityName +
+                " " + stateName +
+                " " + zipCode + "\n";
+
+    }
+
+    public void print() {
+        System.out.println("First Name: " + firstName + " \nLast Name: " + lastName + " \nContact number: "+ phoneNumber + " \nEmail ID: " + emailID + " \nAddress: " + address + " \nCity Name: " + cityName + " \nState Name: " + stateName + " \nPostal Code(Zip Code): " + zipCode + "\n");
+
+    }
+
+}
+

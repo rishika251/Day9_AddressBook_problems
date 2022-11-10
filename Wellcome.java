4c07ff02e2a58d5d6451a4f6dbc0b7517d5d1fc8
rishika251 <rishikasadineni@gmail.com>
2022-11-10 09:12:53 +0530

    Added:wellcome

---
+++ Wellcome.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.AddressBook;
+
+public class Wellcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(" wellcome AddressBook problems");
+	}
+
+}
